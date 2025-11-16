cbf64d0ee7ccc0137dc92cadc371a8959d55e1d5
Joshua <37853883+esqa@users.noreply.github.com>
2025-11-16 23:53:56 +0100

    properly handle encoded types

--- light_map.h
+++ light_map.h
@@ -1,8 +1,8 @@
 #ifndef LIGHT_MAP_H
 #define LIGHT_MAP_H
 
-#include <cassert>
 #include <cstdint>
+#include <cstring>
 
 #if UINTPTR_MAX == 0xFFFFFFFFFFFFFFFF
 #define PLATFORM_64
@@ -15,33 +15,78 @@ assert(!"unknown platform!");
 namespace lmap
 {
 
-class map
-{
-public:
-    template <typename T1, typename T2>
-    map(T1 key, T2 value)
+    class map
     {
+    public:
+        template <typename T1, typename T2>
+        map(T1 key_, T2 value_)
+        {
 #ifdef PLATFORM_32
-        _value = (static_cast<uint64_t>(key) << 32) | static_cast<uint32_t>(value);
+            uint32_t key{}, value{};
+            memcpy(&value, &value_, 4);
+            memcpy(&key, &key_, 4);
+
+            _value = (static_cast<uint64_t>(key) << 32) | value;
 #elif PLATFORM_64
-        _value = (static_cast<unsigned __int128>(key) << 64) | static_cast<uint64_t>(value);
+            uint64_t key{}, value{};
+            memcpy(&value, &value_, 4);
+            memcpy(&key, &key_, 4);
+
+            _value = (static_cast<__int128>(key) << 64) | value;
 #endif
-    }
+        }
 #ifdef PLATFORM_32
-    __forceinline uint32_t upper() const { return _value >> 32; }
-    __forceinline uint32_t lower() const { return _value & 0xFFFFFFFF; }
+        // raw accessor, for encoded types use upper_as
+        __forceinline uint32_t upper() const
+        {
+            return _value >> 32;
+        }
+
+        // raw accessor, for encoded types use lower_as
+        __forceinline uint32_t lower() const
+        {
+            return _value & 0xFFFFFFFF;
+        }
+
 #elif PLATFORM_64
-    __forceinline  uint64_t upper() const { return _value >> 64; }
-    __forceinline  uint64_t lower() const { return _value & 0xFFFFFFFFFFFFFFFF; }
+        // raw accessor, for encoded types use upper_as
+        __forceinline uint64_t upper() const
+        {
+            return _value >> 64;
+        }
+
+        // raw accessor, for encoded types use lower_as
+        __forceinline uint64_t lower() const
+        {
+            return _value & 0xFFFFFFFFFFFFFFFF;
+        }
 #endif
 
-private:
+        template <typename T>
+        __forceinline T upper_as() const
+        {
+            T value{};
+            auto bits = upper();
+            memcpy(&value, &bits, sizeof(T));
+            return value;
+        }
+
+        template <typename T>
+        __forceinline T lower_as() const
+        {
+            T value{};
+            auto bits = lower();
+            memcpy(&value, &bits, sizeof(T));
+            return value;
+        }
+
+    private:
 #ifdef PLATFORM_32
-    uint64_t _value{};
+        uint64_t _value{};
 #elif PLATFORM_64
-    unsigned __int128 _value{};
+        unsigned __int128 _value{};
 #endif
-};
+    };
 
 } // namespace lmap
 
